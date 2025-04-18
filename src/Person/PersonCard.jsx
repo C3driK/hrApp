16ea3fb5aa7c280d263a1c163a656925ad217434
C3driK <olufemiphilips@outlook.com>
2025-04-18 14:34:07 +0300

    add emojis and startDate message

--- src/Person/PersonCard.jsx
+++ src/Person/PersonCard.jsx
@@ -1,17 +1,20 @@
 const PersonCard = (props) => {
   return (
-    <div>
+    <div className="card">
       <p>{props.id}</p>
       <p>{props.name}</p>
       <p>{props.title}</p>
       <p>{props.salary}</p>
       <p>{props.phone}</p>
       <p>{props.email}</p>
-      <p>{props.animal}</p>
+      <p>{props.fanimal}</p>
       <p>{props.startDate}</p>
       <p>{props.location}</p>
       <p>{props.department}</p>
       <p>{props.skills}</p>
+      <div>
+        <p className="reminder">{props.reminder}</p>
+      </div>
     </div>
   );
 };
--- src/Person/PersonList.jsx
+++ src/Person/PersonList.jsx
@@ -2,11 +2,34 @@ import PersonCard from "./PersonCard.jsx";
 import { employees } from "./employeeData.js";
 import { animalEmojis } from "./animalEmoji.js";
 
+const status = (startDateString) => {
+  const today = new Date();
+  const startDate = new Date(startDateString);
+
+  const monthsDiff =
+    (today.getFullYear() - startDate.getFullYear()) * 12 +
+    (today.getMonth() - startDate.getMonth());
+
+  const yearsDiff = today.getFullYear() - startDate.getFullYear();
+
+  if (monthsDiff < 6) {
+    return "🔔 Schedule Probation Review";
+  }
+  if (yearsDiff >= 5 || yearsDiff >= 10 || yearsDiff >= 15) {
+    return "🎉 Schedule recognition meeting.";
+  }
+};
+
 const PersonList = () => {
   return (
     <>
       {employees.map((employee) => (
-        <PersonCard key={employee.id} {...employee} />
+        <PersonCard
+          key={employee.id}
+          fanimal={animalEmojis[employee.animal]}
+          reminder={status(employee.startDate)}
+          {...employee}
+        />
       ))}
     </>
   );
